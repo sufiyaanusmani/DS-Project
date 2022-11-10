217a30a9a2650794fd57197ef896ea5f1eaf0f14
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2022-11-10 22:24:00 +0500

    added class: User

--- Code/main.cpp
+++ Code/main.cpp
@@ -79,6 +79,99 @@ public:
     void readCSV();
 };
 
+class User
+{
+protected:
+    int accountNumber;
+    char name[40];
+    int age;
+    char gender;
+    char email[30];
+    char password[20];
+    virtual int generateAccountNumber() = 0;
+    void inputPassword(char[20]);
+
+public:
+    virtual void createNewAccount() = 0;
+    void setName();
+    void setGender();
+    void setEmail();
+    void setPassword();
+    char *getName();
+    int getAccountNumber();
+    char getGender();
+    int getAge();
+    char *getEmail();
+    virtual void storeData() = 0;
+    void readData();
+    void setAge();
+    virtual void login() = 0;
+    virtual void viewMyInfo() = 0;
+    virtual void portal() = 0;
+    virtual int portalMenu() = 0;
+    virtual void deleteAccount() = 0;
+    virtual void accountSetting() = 0;
+    virtual void updateEmail(int) = 0;
+    virtual void updateInfo(int) = 0;
+    virtual void updateContactNumber(int) = 0;
+    virtual void changePassword(int) = 0;
+};
+
+class Customer : public User
+{
+private:
+    unsigned long long int amount;
+    int generateAccountNumber();
+
+public:
+    void createNewAccount();
+    void depositAmount();
+    void withdrawAmount();
+    void transferAmount();
+    void viewTransactionHistory();
+    void login();
+    void viewMyInfo();
+    void portal();
+    int portalMenu();
+    void storeData();
+    void deleteAccount();
+    unsigned long long int getAmount();
+    void accountSetting();
+    void updateEmail(int);
+    void updateInfo(int);
+    void updateContactNumber(int);
+    void changePassword(int);
+};
+
+class Admin : public User
+{
+private:
+    int generateAccountNumber();
+
+public:
+    void storeData();
+    void login();
+    void viewMyInfo();
+    void portal();
+    int portalMenu();
+    void deleteAccount();
+    void createNewAccount();
+    void createCustomerDatabaseBackup();
+    void createCustomerDatabaseBackupAnimation();
+    void searchCustomer();
+    void searchByAccountNumber();
+    void searchByName();
+    void sortAscending();
+    void sortDescending();
+    void accountSetting();
+    void updateEmail(int);
+    void updateInfo(int);
+    void updateContactNumber(int);
+    void changePassword(int);
+    void generateReport();
+    void viewCustomerAccounts();
+};
+
 int main()
 {
     Properties properties;
@@ -606,4 +699,225 @@ void Properties::readCSV()
         agency = content[i][24];
         append(propertyID, locationID, pageUrl, propertyType, price, priceType, location, city, province, locality, latitude, longitude, baths, area, areaMarla, areaSqft, purpose, bedrooms, dateAdded, year, month, day, agency);
     }
+}
+
+// USER
+
+void User::setName()
+{
+    char ch;
+    int i, backspaceCount;
+    backspaceCount = i = 0;
+    cout << "Enter your full name: ";
+    while (1)
+    {
+        ch = getch();
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch == 32))
+        {
+            this->name[i] = ch;
+            cout << ch;
+            i++;
+            backspaceCount++;
+        }
+        else if (ch == 8 && backspaceCount > 0)
+        {
+            cout << "\b \b";
+            i--;
+            backspaceCount--;
+        }
+        else if (ch == 13)
+        {
+            break;
+        }
+    }
+    this->name[i] = '\0';
+}
+
+void User::setAge()
+{
+    char ch, a[3];
+    int i, backspaceCount;
+    backspaceCount = i = 0;
+    cout << "Enter your age : ";
+    while (1)
+    {
+        ch = getch();
+        if (ch >= '0' && ch <= '9')
+        {
+            a[i] = ch;
+            cout << ch;
+            i++;
+            backspaceCount++;
+        }
+        else if (ch == 8 && backspaceCount > 0)
+        {
+            cout << "\b \b";
+            i--;
+            backspaceCount--;
+        }
+        else if (ch == 13 && i == 2)
+        {
+            break;
+        }
+    }
+    a[3] = '\0';
+    this->age = ((a[0] - 48) * 10) + (a[1] - 48);
+    if (this->age < 18)
+    {
+        TextColor(4);
+        cout << "\n\t\t\t\t\tAge must be greater than or equal to 18" << endl;
+        TextColor(7);
+        setAge();
+    }
+}
+
+void User::setGender()
+{
+    char ch;
+    cout << "Enter your gender (m:male, f:female): ";
+    while (1)
+    {
+        ch = getch();
+        if (ch == 'm' || ch == 'f')
+        {
+            this->gender = ch;
+            cout << ch;
+            break;
+        }
+    }
+}
+
+void User::setEmail()
+{
+    char ch;
+    int i, backspaceCount;
+    backspaceCount = i = 0;
+    cout << "Enter your email: ";
+    while (1)
+    {
+        ch = getch();
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch == 32) || (ch == '@') || (ch == '.') || (ch >= '0' && ch <= '9'))
+        {
+            this->email[i] = ch;
+            cout << ch;
+            i++;
+            backspaceCount++;
+        }
+        else if (ch == 8 && backspaceCount > 0)
+        {
+            cout << "\b \b";
+            i--;
+            backspaceCount--;
+        }
+        else if (ch == 13)
+        {
+            break;
+        }
+    }
+    this->email[i] = '\0';
+}
+
+void User::setPassword()
+{
+    char ch;
+    int i, backspaceCount;
+    backspaceCount = i = 0;
+    cout << "Enter your password: ";
+    TextColor(4);
+    while (1)
+    {
+        ch = getch();
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch == 32) || (ch == '@') || (ch == '.') || (ch >= '0' && ch <= '9'))
+        {
+            this->password[i] = ch;
+            cout << '*';
+            i++;
+            backspaceCount++;
+        }
+        else if (ch == 8 && backspaceCount > 0)
+        {
+            cout << "\b \b";
+            i--;
+            backspaceCount--;
+        }
+        else if (ch == 13)
+        {
+            break;
+        }
+    }
+    this->password[i] = '\0';
+    TextColor(15);
+}
+
+void User::readData()
+{
+    ifstream fin;
+    system("cls");
+    fin.open("./data/customer.bank", ios::in | ios::binary);
+    fin.read((char *)this, sizeof(*this));
+    while (fin.eof() == 0)
+    {
+        cout << "Acc No: " << this->accountNumber << endl;
+        cout << "Name: " << this->name << endl;
+        cout << "Email: " << this->email << endl;
+        cout << endl;
+        fin.read((char *)this, sizeof(*this));
+    }
+    fin.close();
+}
+
+void User::inputPassword(char pass[20])
+{
+    char ch;
+    int i, backspaceCount;
+    backspaceCount = i = 0;
+    TextColor(4);
+    while (1)
+    {
+        ch = getch();
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch == 32) || (ch == '@') || (ch == '.') || (ch >= '0' && ch <= '9'))
+        {
+            pass[i] = ch;
+            cout << '*';
+            i++;
+            backspaceCount++;
+        }
+        else if (ch == 8 && backspaceCount > 0)
+        {
+            cout << "\b \b";
+            i--;
+            backspaceCount--;
+        }
+        else if (ch == 13)
+        {
+            break;
+        }
+    }
+    TextColor(15);
+    pass[i] = '\0';
+}
+
+int User::getAccountNumber()
+{
+    return accountNumber;
+}
+
+char *User::getName()
+{
+    return name;
+}
+
+int User::getAge()
+{
+    return age;
+}
+
+char User::getGender()
+{
+    return gender;
+}
+
+char *User::getEmail()
+{
+    return email;
 }
