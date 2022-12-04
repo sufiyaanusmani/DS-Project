9ddb696368bc4c60ce6d8b614a88fb723c6f3f95
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2022-12-04 11:42:11 +0500

    remove unnecessary code

--- Code/main.cpp
+++ Code/main.cpp
@@ -206,35 +206,6 @@ class EmailQueue{
         void printQueue();
 };
 
-// class Admin : public User
-// {
-// private:
-//     int generateAccountNumber();
-
-// public:
-//     void storeData();
-//     void login();
-//     void viewMyInfo();
-//     void portal();
-//     int portalMenu();
-//     void deleteAccount();
-//     void createNewAccount();
-//     void createCustomerDatabaseBackup();
-//     void createCustomerDatabaseBackupAnimation();
-//     void searchCustomer();
-//     void searchByAccountNumber();
-//     void searchByName();
-//     void sortAscending();
-//     void sortDescending();
-//     void accountSetting();
-//     void updateEmail(int);
-//     void updateInfo(int);
-//     void updateContactNumber(int);
-//     void changePassword(int);
-//     void generateReport();
-//     void viewCustomerAccounts();
-// };
-
 int main()
 {
     int mainMenuChoice;
